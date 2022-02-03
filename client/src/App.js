dc36fa5b638ccddbef91807792f0bb4770e167f6
Artur Krauze <apkrauze@gmail.com>
2022-02-03 23:07:47 +0100

    Created client side with two inputs, enter button, installed socket.io-client , created a simple room connection to the server. On the server side installed express, cors and socket.io, created simole connection between server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,49 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+let socket;
+const CONNECTION_PORT = "localhost:3002/";
 
 function App() {
-  return <div className="App"></div>;
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [room, setRoom] = useState("");
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT, { transports: ["websocket"] });
+  }, [CONNECTION_PORT]);
+
+  const connectToRoom = () => {
+    socket.emit("join_room", room);
+  };
+  return (
+    <div className="App">
+      {!loggedIn ? (
+        <div className="logIn">
+          <div className="inputs">
+            <input
+              type="text"
+              placeholder="Name..."
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="ChatRoom..."
+              onChange={(e) => {
+                setRoom(e.target.value);
+              }}
+            />
+          </div>
+          <button onClick={connectToRoom}>Enter Chat</button>
+        </div>
+      ) : (
+        <h1>You Are Logged In</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
